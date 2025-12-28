65f549be2ff554b9ef6bad66bbbeefe771f5609e
Kamya Tanhaie <dgghost034@gmail.com>
2025-12-28 17:06:20 +0330

    just committing

--- src/Body.jsx
+++ src/Body.jsx
@@ -5,14 +5,24 @@ const Body = () => {
         className="w-25 bg-secondary mx-2 rounded"
         style={{ height: "calc(100vh - 200px)" }}
       >
-        <div className="d-flex justify-content-end px-2 py-1">X</div>
+        <div
+          className="d-flex justify-content-end px-2 py-1"
+          style={{ cursor: "pointer" }}
+        >
+          X
+        </div>
         <div></div>
       </div>
       <div
         className="w-25 bg-secondary mx-2 rounded"
         style={{ height: "calc(100vh - 200px)" }}
       >
-        <div className="d-flex justify-content-end px-2 py-1">X</div>
+        <div
+          className="d-flex justify-content-end px-2 py-1 "
+          style={{ cursor: "pointer" }}
+        >
+          X
+        </div>
         <div></div>
       </div>
     </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,12 @@
 const Header = () => {
   return (
     <header className="d-flex justify-content-between align-items-center p-4 mx-4 mt-4 rounded bg-danger">
-      <div>🍿usepopcorn</div>
+      <div className="d-flex align-items-center">
+        🍿
+        <p className="h3 ms-2">usepopcorn</p>
+      </div>
       <div>
-        <input type="text" placeholder="Search movie..." />
+        <input className="form-control w-100" type="text" placeholder="Search movie..." />
       </div>
       <div>
         <p className="mb-0 text-white">Found 0 top results</p>
