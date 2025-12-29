8618a33e940c157cc5c0e7ff5d8f5e8b905f213c
Kamya Tanhaie <dgghost034@gmail.com>
2025-12-29 12:20:28 +0330

    adding show/hide action

--- src/ListBox.jsx
+++ src/ListBox.jsx
@@ -1,12 +1,21 @@
 import MovieList from "./MovieList";
+import { useState } from "react";
 
 const ListBox = ({ movies }) => {
+  const [showMoies, setShowMovies] = useState(true);
   return (
     <div
       className="d-flex flex-column justify-content-center align-items-center p-4 rounded bg-black text-white"
       style={{ width: "400px" }}
     >
-      <MovieList movies={movies}></MovieList>
+      <div
+        onClick={() => setShowMovies(!showMoies)}
+        className="d-flex align-self-end mb-2"
+        style={{ cursor: "pointer" }}
+      >
+        {showMoies ? "X" : "-"}
+      </div>
+      {showMoies && <MovieList movies={movies} />}
     </div>
   );
 };
--- src/Movie.jsx
+++ src/Movie.jsx
@@ -4,7 +4,7 @@ const Movie = ({ movie }) => {
     <li className="d-flex" style={{ listStyle: "none" }}>
       <img className="me-4 mb-3" src={movie.Poster} style={{ width: "65px" }} alt={movie.Title}/>
       <div className="d-flex flex-column  justify-content-center">
-        <h4>{movie.Title}</h4>
+        <h5>{movie.Title}</h5>
         <p>
           <span>📅</span>
           <span>{movie.Year}</span>
